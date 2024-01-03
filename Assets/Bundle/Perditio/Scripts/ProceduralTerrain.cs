e9ad840f98e894d196a89686eca879a25d63a4a3
Huambo29 <bitru29@gmail.com>
2024-01-03 23:36:20 +0100

    only works on host

--- Assets/Bundle/Perditio/Scripts/ProceduralTerrain.cs
+++ Assets/Bundle/Perditio/Scripts/ProceduralTerrain.cs
@@ -9,35 +9,20 @@ public class ProceduralTerrain : NetworkBehaviour
     MeshRenderer mesh_renderer;
     MeshCollider mesh_collider;
 
+    [HideInInspector]
+    public Game.Map.Battlespace battlespace;
+    [HideInInspector]
+    public Material terrain_material;
 
-    [SerializeField]
-    bool debug = false;
+    [HideInInspector]
+    public int grid_resolution = 100;
+    [HideInInspector]
+    public float mesh_size = 100f;
+    [HideInInspector]
+    public float density_cut_off = 0.5f;
 
-    [SerializeField]
-    Material terrain_material;
-    //[SerializeField]
-    //Material tacview_material;
-
-    [SerializeField]
-    int grid_resolution = 100;
-
-    [SerializeField]
-    float mesh_size = 100f;
-
-    [SerializeField]
-    float density_cut_off = 0.5f;
-
-    [SerializeField]
-    float drop_off_start = 30f;
-
-    [SerializeField]
-    float drop_off_end = 40f;
-
-    [SyncVar(hook = "UpdateMesh")]
-    float[,,] density_field;
-
-    [SyncVar(hook = "UpdateCapPoints")]
-    Vector3[] DistributedObjectives;
+    [HideInInspector]
+    public float[,,] density_field;
 
     Vector3 GetPositionFromGrid(Vector3Int root_position)
     {
@@ -235,15 +220,6 @@ public class ProceduralTerrain : NetworkBehaviour
             }
         }
 
-        //new_mesh.vertices = new Vector3[] {
-        //    new Vector3(100f, 0f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 100f, 0f),
-        //    new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 100f), new Vector3(0f, 100f, 0f),
-        //    new Vector3(0f, 0f, 0f), new Vector3(100f, 0f, 0f), new Vector3(0f, 0f, 100f),
-        //    new Vector3(100f, 0f, 0f), new Vector3(0f, 100f, 0f), new Vector3(0f, 0f, 100f)
-        //
-        //};
-        //new_mesh.triangles = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-
         Mesh new_mesh = new Mesh();
         new_mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         new_mesh.vertices = vertices.ToArray();
@@ -255,226 +231,7 @@ public class ProceduralTerrain : NetworkBehaviour
         return new_mesh;
     }
 
-    [SerializeField]
-    float fractal_global_scale = 200f;
-    [SerializeField]
-    int Iterations = 14;
-    [SerializeField]
-    float fractal_scale = 2f;
-    [SerializeField]
-    float minRadius2 = 0.44f;
-    [SerializeField]
-    float fixedRadius2 = 5.48f;
-    [SerializeField]
-    float foldingLimit = 1.33f;
-    [SerializeField]
-    float Repetysion = 0f;
-    [SerializeField]
-    float fractal_minimum = 0.01f;
-    [SerializeField]
-    Vector3 fractal_offset;
-    [SerializeField]
-    Quaternion fractal_rotation;
-
-    void sphereFold(ref Vector3 z, ref float dz)
-    {
-        float r2 = Vector3.Dot(z, z);
-        if (r2 < minRadius2)
-        {
-            // linear inner scaling
-            float temp = (fixedRadius2 / minRadius2);
-            z *= temp;
-            dz *= temp;
-        }
-        else if (r2 < fixedRadius2)
-        {
-            // this is the actual sphere inversion
-            float temp = (fixedRadius2 / r2);
-            z *= temp;
-            dz *= temp;
-        }
-    }
-
-    void boxFold(ref Vector3 z, ref float dz)
-    {
-        z.x = Mathf.Clamp(z.x, -foldingLimit, foldingLimit) * 2f - z.x;
-        z.y = Mathf.Clamp(z.y, -foldingLimit, foldingLimit) * 2f - z.y;
-        z.z = Mathf.Clamp(z.z, -foldingLimit, foldingLimit) * 2f - z.z;
-    }
-
-
-    float FractalDistanceFunction(Vector3 p)
-    {
-        p /= fractal_global_scale;
-
-        if (Repetysion != 0)
-        {
-            p.x = p.x % Repetysion;
-            p.y = p.y % Repetysion;
-            p.y = p.z % Repetysion;
-
-            p.x -= Mathf.Sign(p.x) * Repetysion / 2;
-            p.y -= Mathf.Sign(p.y) * Repetysion / 2;
-            p.z -= Mathf.Sign(p.z) * Repetysion / 2;
-        }
-
-        Vector3 offset = p;
-        float dr = 1f;
-        for (int n = 0; n < Iterations; n++)
-        {
-            boxFold(ref p, ref dr); 
-            sphereFold(ref p, ref dr);
-
-            p = fractal_scale * p + offset;
-            dr = dr * Mathf.Abs(fractal_scale) + 1f;
-        }
-        float r = p.magnitude;
-        return r / Mathf.Abs(dr) * fractal_global_scale;
-    }
-
-    float SamplePerlinNoise(Vector3 pos)
-    {
-        float ab = Mathf.PerlinNoise(pos.x, pos.y);
-        float bc = Mathf.PerlinNoise(pos.y, pos.z);
-        float ac = Mathf.PerlinNoise(pos.x, pos.z);
-
-        float ba = Mathf.PerlinNoise(pos.y, pos.x);
-        float cb = Mathf.PerlinNoise(pos.z, pos.y);
-        float ca = Mathf.PerlinNoise(pos.z, pos.x);
-
-        return (ab + bc + ac + ba + cb + ca) / 6f;
-    }
-
-    [SerializeField]
-    int octaves = 4;
-    [SerializeField]
-    Vector3 perlin_offset = new Vector3(1123f, 12f, 5000f);
-    [SerializeField]
-    Quaternion perlin_rotation;
-    [SerializeField]
-    float perlin_scale = 100f;
-    [SerializeField]
-    float persistence = 0.5f;
-    [SerializeField]
-    float lacunarity = 2f;
-
-
-    float PerlinOctaves(Vector3 pos)
-    {
-        float result = 0f;
-
-        float amplitude = 1f;
-        float frequency = 1f;
-
-        for (int i = 0; i < octaves; i++)
-        {
-            result += amplitude * SamplePerlinNoise(pos * frequency);
-            amplitude *= persistence;
-            frequency *= lacunarity;
-        }
-
-        if (1f - persistence == 0)
-        {
-            persistence = 1.000001f;
-        }
-
-        return result / ((1f - Mathf.Pow(persistence, octaves)) / (1f - persistence));
-    }
-
-    float interest_points_drop_off(Vector3 pos)
-    {
-        float result = 0f;
-
-        float cap_drop_off_start = 60f;
-        float cap_drop_off_end = 150f;
-
-
-        Vector3 sample_grid_positon = pos / grid_resolution - Vector3.one * 0.5f;
-
-        Game.Map.Battlespace battlespace = gameObject.GetComponentInParent<Game.Map.Battlespace>();
-        for (int i = 0; i < battlespace.DistributedObjectives.Length; i++)
-        {
-            Vector3 cap_grid_position = battlespace.DistributedObjectives[i] / mesh_size;
-
-            float center_distance = Vector3.Distance(sample_grid_positon, cap_grid_position) * mesh_size;
-            //Debug.Log("center_distance: " + center_distance);
-            float drop_off = 1f - ((Mathf.Max(cap_drop_off_start, Mathf.Min(center_distance, cap_drop_off_end)) - cap_drop_off_start) / (cap_drop_off_end - cap_drop_off_start));
-            result -= Mathf.Pow(drop_off, 2f);
-        }
-
-        return result;
-    }
-
-    float CalculateDensity(int x, int y, int z)
-    {
-        float center_distance = Mathf.Sqrt(Mathf.Pow((float)x / grid_resolution - 0.5f, 2f) + Mathf.Pow((float)y / grid_resolution - 0.5f, 2f) + Mathf.Pow((float)z / grid_resolution - 0.5f, 2f)) * mesh_size;
-        float drop_off = ((Mathf.Max(drop_off_start, Mathf.Min(center_distance, drop_off_end)) - drop_off_start) / (drop_off_end - drop_off_start));
-
-        return PerlinOctaves((perlin_rotation * new Vector3(x, y, z) + perlin_offset) * 1.41421356237f / perlin_scale) - Mathf.Pow(drop_off, 2f) + interest_points_drop_off(new Vector3(x, y, z));
-
-        // return 1f - FractalDistanceFunction(fractal_rotation * new Vector3((float)x / grid_resolution - 0.5f, (float)y / grid_resolution - 0.5f, (float)z / grid_resolution - 0.5f) + fractal_offset) / mesh_size;
-    }
-
-    public override void OnStartServer()
-    {
-        Debug.Log("OnStartServer " + gameObject.GetInstanceID());
-        base.OnStartServer();
-        Debug.Log("OnStartServer2");
-        Game.Map.Battlespace battlespace = gameObject.GetComponentInParent<Game.Map.Battlespace>();
-        for (int i = 0; i < battlespace.DistributedObjectives.Length; i++)
-        {
-            if (i == 0)
-            {
-                battlespace.DistributedObjectives[i] = Random.onUnitSphere * Random.Range(0f, mesh_size / 2f * 0.1f);
-            }
-            else if (i % 2 == 1)
-            {
-                bool good_placement;
-                do
-                {
-                    battlespace.DistributedObjectives[i] = Random.onUnitSphere * Random.Range(mesh_size / 2f * 0.1f + 400f, mesh_size / 2f - 200f);
-                    good_placement = true;
-                    for (int k = 0; k < i; k++)
-                    {
-                        if (Vector3.Distance(battlespace.DistributedObjectives[k], battlespace.DistributedObjectives[i]) <= 500f)
-                        {
-                            good_placement = false;
-                            break;
-                        }
-                    }
-                } while (!good_placement);
-            }
-            else
-            {
-                battlespace.DistributedObjectives[i] = -battlespace.DistributedObjectives[i - 1];
-            }
-        }
-
-        float[,,] new_density_field = new float[grid_resolution, grid_resolution, grid_resolution];
-
-        for (int x = 0; x < grid_resolution; x++)
-        {
-            for (int y = 0; y < grid_resolution; y++)
-            {
-                for (int z = 0; z < grid_resolution; z++)
-                {
-                    new_density_field[x, y, z] = CalculateDensity(x, y, z);
-
-                    if (
-                        x == 0 || x == grid_resolution - 1 ||
-                        y == 0 || y == grid_resolution - 1 ||
-                        z == 0 || z == grid_resolution - 1
-                    )
-                    {
-                        new_density_field[x, y, z] = 0f;
-                    }
-                }
-            }
-        }
-
-        density_field = new_density_field;
-    }
-
+    /*
     private void UpdateMesh(
         float[,,] old_value,
         float[,,] new_value
@@ -491,30 +248,17 @@ public class ProceduralTerrain : NetworkBehaviour
         mesh_filter.mesh = new_mesh;
         mesh_collider.sharedMesh = new_mesh;
     }
-
-    private void UpdateCapPoints(Vector3[] old_value, Vector3[] new_value)
-    {
-        Debug.Log("UpdateCapPoints");
-        Game.Map.Battlespace battlespace = gameObject.GetComponentInParent<Game.Map.Battlespace>();
-
-        for (int i = 0; i < battlespace.DistributedObjectives.Length; i++)
-        {
-            battlespace.DistributedObjectives[i] = new_value[i];
-        }
-    }
+    */
 
     void Start()
     {
         if (!NetworkServer.active)
         {
-            Debug.Log("Server not active. Going into editor mode");
-        } else
-        {
-            NetworkServer.Spawn(gameObject);
+            Debug.Log("You are not server");
+            return;
         }
-        
-        Debug.Log("Starte");
-        Debug.Log("isServer: " + isServer + " isServerOnly: " + isServerOnly + " isClient: " + isClient + " isClientOnly: " + isClientOnly + " isLocalPlayer: " + isLocalPlayer);
+
+        Debug.Log("Terrain isServer: " + isServer + " isServerOnly: " + isServerOnly + " isClient: " + isClient + " isClientOnly: " + isClientOnly + " isLocalPlayer: " + isLocalPlayer);
 
         mesh_filter = gameObject.GetComponent<MeshFilter>();
         mesh_renderer = gameObject.GetComponent<MeshRenderer>();
@@ -522,28 +266,10 @@ public class ProceduralTerrain : NetworkBehaviour
 
         mesh_renderer.material = terrain_material;
 
-        if (isServer || !NetworkServer.active)
-        {
-            perlin_offset = new Vector3(Random.Range(-100000f, 100000f), Random.Range(-100000f, 100000f), Random.Range(-100000f, 100000f));
-            perlin_rotation = Random.rotation;
-            density_cut_off = 0.5f - 0.02f * Random.value;
+        Mesh new_mesh = GenerateMesh();
+        mesh_filter.mesh = new_mesh;
+        mesh_collider.sharedMesh = new_mesh;
 
-            OnStartServer();
-
-            Mesh new_mesh = GenerateMesh();
-            mesh_filter.mesh = new_mesh;
-            mesh_collider.sharedMesh = new_mesh;
-        } 
-    }
-
-    private void Update()
-    {
-        if (!this.isServer)
-        {
-            return;
-        } else
-        {
-            Debug.Log("I am Server" + gameObject.GetInstanceID());
-        }
+        Debug.Log("Density sample: " + density_field[32, 32, 32]);
     }
 }
